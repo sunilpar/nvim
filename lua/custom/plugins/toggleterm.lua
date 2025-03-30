be3652977b82cc0f41db83fd76c7d6621ed913c9
sunilpar <sunilformal42@gmail.com>
2025-03-30 02:11:34 +0000

    added a terminal with ctrl+j like in  vscode config

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				size = 15,
+				open_mapping = [[<C-j>]],
+				direction = "horizontal",
+				hide_numbers = true,
+				shade_filetypes = {},
+				shade_terminals = true,
+				shading_factor = 2,
+				start_in_insert = true,
+				insert_mappings = true,
+				persist_size = true,
+				close_on_exit = true,
+				shell = vim.o.shell,
+			})
+		end,
+	},
+}
