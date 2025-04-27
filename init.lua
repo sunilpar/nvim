0945a7836f9dac07fac4261e41ed194bc49cfb9a
sunilpar <sunilformal42@gmail.com>
2025-04-27 00:26:38 +0000

    added zshconfig file as a backup

--- init.lua
+++ init.lua
@@ -101,8 +101,6 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
-
 -- for go XD
 vim.keymap.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
 vim.keymap.set("n", "<leader>pf", 'ofmt.Printf("\\n", )<Esc>F\\i')
